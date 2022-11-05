9bec8b62f59cb33e185d1cc36379c56e4d217123
Rodrigo Lima Cortes <69279039+MalfCortes@users.noreply.github.com>
2022-11-05 16:30:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const root = document.querySelectorid("#root");
 const canvas = document.querySelector('#canvas');
-const ctx = main.getContext('2d');
+const ctx = canvas.getContext('2d');
+
+root.innerHTML = "Rodrigo"
+
 ctx.fillStyle = 'green';
 ctx.fillrect(0,0, 50, 50);
