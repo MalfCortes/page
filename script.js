09f6413b1cd302cd96a97a6ea8b87d4b084b2635
Rodrigo Lima Cortes <69279039+MalfCortes@users.noreply.github.com>
2022-11-05 17:23:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-const obj = {x:20, y:20, t:100}
+const obj = {x:20, y:20, t:100, s:1}
 
 const mostra = () =>{
     ctx.clearRect(0, 0, 300, 150);
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(obj.x, obj.y, obj.t, obj.t);
-    obj.x++;
+    obj.x += obj.s;
+    if(obj.x + obj.t > 300 || obj.x < 0) obj.s *= -1
 }
 
-setInterval(mostra, 100);
+setInterval(mostra, 15);
